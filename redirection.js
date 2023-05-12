40ec2aac1f9771d09f4fe8bf83461e4fba17f6f8
jumia-ng-electronics-category-nig <131971835+jumia-ng-electronics-category-nig@users.noreply.github.com>
2023-05-12 20:22:50 +0100

    Create redirection.js

---
+++ redirection.js
@@ -0,0 +1,3 @@
+if (window.location.href === 'https://jumia-ng-electronics-category-nig.github.io/jumia.com.ng/loading-page.jpg') {
+  window.location.href = 'https://jumia-ng-electronics-category-nig.github.io/jumia.com.ng/index0.2.html';
+}
