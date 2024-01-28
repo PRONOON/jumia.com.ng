4d0ca616108fae1bbbcf8c8128b6f43aede6424c
PRONOON <131971835+PRONOON@users.noreply.github.com>
2024-01-28 01:42:08 +0100

    Delete redirection.js

--- redirection.js
+++
@@ -1,3 +0,0 @@
-if (window.location.href === 'https://jumia-ng-electronics-category-nig.github.io/jumia.com.ng/loading-page.jpg') {
-  window.location.href = 'https://jumia-ng-electronics-category-nig.github.io/jumia.com.ng/index0.2.html';
-}
